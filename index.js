282a3b5b1010db7c4908ce27ba5be8c888243880
Omid <mail.dev.om@gmail.com>
2024-08-26 16:46:06 +0330

    1 deploy

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const { Telegraf } = require('telegraf')
 const { message } = require('telegraf/filters');
 const dotenv = require('dotenv');
+const express = require('express');
 
 
 dotenv.config();
+const app = express();
+const port = process.env.PORT;
 const botToken = process.env.BOT_TOKEN;
-const webLink = process.env.WEB_LINK
-console.log(botToken);
+const webLink = process.env.WEB_LINK;
+
 const bot = new Telegraf(botToken);
+app.use(express.json());
+
+app.get('*', async (req, res) => {
+    res.send('hello get');
+    console.log(req.body);
+});
+app.post('*', async (req, res) => {
+    res.send('hello get');
+});
+
+app.listen(port, function () {
+    console.log(`app is running on port ${port}`);
+});
 
 
 // bot.start((ctx) => ctx.reply('welcome', {
