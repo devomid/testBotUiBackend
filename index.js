640523b34d337657060ff21f22d486b291170e53
Omid <mail.dev.om@gmail.com>
2024-08-27 13:56:55 +0330

    jkkjdssj

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Telegraf } = require('telegraf')
 const { message } = require('telegraf/filters');
 const dotenv = require('dotenv');
 const express = require('express');
+const cors = require('cors');
 
 
 dotenv.config();
@@ -12,6 +13,14 @@ const webLink = process.env.WEB_LINK;
 
 const bot = new Telegraf(botToken);
 app.use(express.json());
+
+app.use(cors({
+    credentials: true,
+    origin: ['https://testbotuifrontend.onrender.com',
+        'http://localhost:5005'
+    ],
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+}));
 
 app.get('*', async (req, res) => {
     res.send('hello get');
